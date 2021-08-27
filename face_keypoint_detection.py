ce4bbbcee204a3f97471dafcb0ed249395e399c7
Kishani Kandasamy <53715187+KishaniKandasamy@users.noreply.github.com>
2021-08-27 23:50:16 +0530

    perform some datacleaning

--- face_keypoint_detection.py
+++ face_keypoint_detection.py
@@ -1,1 +1,39 @@
+import pandas as pd
+import random
+import numpy as np
+import os
+import PIL
+import seaborn as sns
+import pickle
+from PIL import *
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from IPython.display import display
+from tensorflow.python.keras import *
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, optimizers
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import *
+from tensorflow.keras import backend as K
+from keras import optimizers
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
+keyfacial_df = pd.read_csv('./../data.csv')
+keyfacial_df.info()
+
+# Check if null values exist in the dataframe
+keyfacial_df.isnull().sum()
+
+#initially 1D array
+keyfacial_df['Image'].shape
+
+#Image are given as space separated string, separate the values using ' ' as separator. 1D->2D
+keyfacial_df['Image'] = keyfacial_df['Image'].apply(lambda x: np.fromstring(x, dtype = int, sep = ' ').reshape(96, 96))
+keyfacial_df['Image'][0].shape
